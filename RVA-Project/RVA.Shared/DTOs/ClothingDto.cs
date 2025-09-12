7a04eb9c4bacb9c27aeba93d8fc912bfcd4a3020
aleksamilicev <amilicev92@gmail.com>
2025-09-12 13:51:11 +0200

    zaboravio sam da uradim save ova 2 fajla pa radim dodatni commit

--- RVA-Project/RVA.Shared/DTOs/ClothingDto.cs
+++ RVA-Project/RVA.Shared/DTOs/ClothingDto.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RVA.Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
