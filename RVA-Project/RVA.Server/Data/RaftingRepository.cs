baf480c2758f53ab508b1d08d33dcf0cef4a53af
aleksamilicev <amilicev92@gmail.com>
2025-09-12 12:28:59 +0200

    Updated folders
    Dodao sam nove fajlove koje planiram da koristim prilikom implementacije

---
+++ RVA-Project/RVA.Server/Data/RaftingRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RVA.Server.Data
+{
+    public class RaftingRepository
+    {
+    }
+}
---
+++ RVA-Project/RVA.Server/Storage/JsonStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RVA.Server.Storage
+{
+    public class JsonStorage
+    {
+    }
+}
